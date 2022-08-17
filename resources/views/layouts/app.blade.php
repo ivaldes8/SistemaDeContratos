ee2b882676f7eb2d41ab1fd0ea5591c1049d9bb3
Ivan Gonzalez <kaosolution8@gmail.com>
2022-08-16 22:44:44 -0400

    fixes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Plantel-Industrial') }}</title>
+    <title>{{ config('app.name', 'Contratacion') }}</title>
     <link rel="icon" type="image/x-icon" href="{{ asset('1.ico') }}">
 
     <!-- Scripts -->
